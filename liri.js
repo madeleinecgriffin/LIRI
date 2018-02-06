feb212cc91e413ab3679e2e0fd8e6f1857c74d01
Madeleine Griffin <madeleinecgriffin@gmail.com>
2018-02-05 19:14:47 -0600

    adding switch case for different actions

--- liri.js
+++ liri.js
@@ -5,28 +5,40 @@ var keys = require('./keys')
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 
-//var spotify = new Spotify(keys.spotify);
-
-console.log(keys)
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
-
-
-//tweet search
-
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
-   console.log(tweets);
-});
+var inputCommand = process.argv[2];
+var inputCommandName = process.argv[3];
 
 
 
-//spotify search
-/*
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});*/
+switch(inputCommand) {
+
+	case "my-tweets":
+
+		client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+   			console.log(tweets);
+		});
+		break
+
+	case "spotify-this-song":
+		
+		spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			}
+			console.log(data); 
+		});
+		break
+
+	case "movie-this":
+		break
+	case "do-what-it-says":
+		break
+	default:
+		console.log("please enter a valid command");
+		break
+}
+
