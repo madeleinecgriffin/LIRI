47bf64cc32335dafb9b698749317d8537e4942af
Madeleine Griffin <madeleinecgriffin@gmail.com>
2018-02-06 21:50:03 -0600

    fin - all commands working and looking pretty

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # LIRI
 NWCB assignment - making a basic app similar to SIRI
+
+In this App, I use Node.js to process several different search commands that could be given by the user.
+
+If a user's command is "my-tweets", the app will look up the last 20 tweets created by the dummy Twitter account @retweet_cats and print the text of the last 20 tweets.
+
+If the user's command is "spotify-this-song", the app will look up the top 20 search items for this song (could be song name, album name, band name, etc.) In this case the user will need to input a second command to specify the search term they would like the app to look for.
+
+If the user's command is "movie-this", the app will print a movie summary from OMDB for that movie. In this case the user will have to input a second command to specify the movie they would like to look up as well.
+
+If the user's command is "do-what-it-says", the app will read a text file. The text file will contain commands relevant to the above tasks - for example, it could consists of "spotify-this-song" followed by a song name. The app will then process the command written in the text file. In this case a second input is needed to specify the text file the app should read.
+
+If the user does not input any of these commands, the app will prompt them to do so. Additionally, the app prompts the user to input more information if not enough is given.
--- liri.js
+++ liri.js
@@ -14,46 +14,56 @@ var client = new Twitter(keys.twitter);
 //store user inputs
 var inputCommand = process.argv[2];
 var inputCommandName = process.argv[3];
+var count = 0;
 
 
 //functions for each task
 var getTweets = function() {
 	client.get('statuses/user_timeline', {q: 'retweet_cats'}, function(error, tweets, response) {
-		console.log(tweets);
+		
+		//prints out the text of the tweets in an account
 		for(var i = 0; i < tweets.length; i++) {
 			var tweet = tweets[i];
+			var count = i + 1
+			console.log("-------------------------")
+			console.log("Tweet Number: " + count);
 			console.log("Tweet Text: " + tweet.text);
 		}
-
+		console.log("-------------------------")
 	});
 }
 
 var getSong = function() {
 	spotify.search({ type: 'track', query: inputCommandName }, function(err, data) {
+		
 		if (err) {
 			return console.log('Error occurred: ' + err);
 		}
 
-		//console.log(data);
-		//var song = data.tracks.items[0];
-		//console.log(song)
+		//a for loop that finds top search results for a keyword in a song or album
 		for(var i = 0; i < data.tracks.items.length; i++) {
 			var song = data.tracks.items[i];
+			var count = i + 1
+			console.log("-------------------------")
+			console.log("Track Number: " + count);
 			console.log("Song: " + song.name);
 			console.log("Artist Name: " + song.artists[0].name);
 			console.log("Preview URL: " + song.preview_url);
 			console.log("Album: " + song.album.name); 
 		}
+		console.log("-------------------------")
 	});
 }
 
 var getMovie = function() {
 	request("http://www.omdbapi.com/?t=" + inputCommandName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-  		console.log('error:', error); // Print the error if one occurred
-  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  		console.log('body:', body); // Print the HTML for the Google homepage.
   		
+  		console.log('error:', error);
+  		console.log('statusCode:', response && response.statusCode); 
+  		
+  		//parses data into readable object and prints information about the movie
   		var parseBody = JSON.parse(body);
+  		console.log("-------------------------")
   		console.log("Movie Title: " + parseBody.Title);
   		console.log("Year: " + parseBody.Year);
   		console.log("IMDB Rating: " + parseBody.imdbRating);
@@ -62,38 +72,73 @@ var getMovie = function() {
   		console.log("Language: " + parseBody.Language);
   		console.log("Plot: " + parseBody.Plot);
   		console.log("Starring: " + parseBody.Actors);
+  		console.log("-------------------------")
   	});
 }
 
 var doTask = function() {
 	fs.readFile(inputCommandName, 'utf8', function(err, data) {
 		if (err) throw err;
-		console.log('OK: ' + inputCommandName);
-		console.log(data)		
+
+		//splits text of file into an array and assigns those values to the new input commands and reruns the command function
+		var newArr = data.split(",");
+		inputCommand = newArr[0];
+		inputCommandName = newArr[1];
+		readInput();
 	})
 }
 
-//determines which task to complete based on user input
-switch(inputCommand) {
 
-	case "my-tweets":
+
+//determines which task to complete based on user input
+var readInput = function() {
+	
+	switch(inputCommand) {
+
+		case "my-tweets":
 		getTweets();
 		break
 
-	case "spotify-this-song":
-		getSong();
+		case "spotify-this-song":
+		if (inputCommandName) {
+			getSong();
+		}
+		else {
+			console.log("-------------------------")
+			console.log("Please specify the keyword you would like to use to search Spotify after you request 'spotify-this-song'.");
+			console.log("-------------------------")
+		}
 		break
 
-	case "movie-this":
-		getMovie();
+		case "movie-this":
+		if (inputCommandName) {
+			getMovie();
+		}
+		else {
+			console.log("-------------------------")
+			console.log("Please specify the movie you would like to search for after you request 'movie-this'.");
+			console.log("-------------------------")
+		}
 		break
 
-	case "do-what-it-says":
-		doTask();
+		case "do-what-it-says":
+		if (inputCommandName) {
+			doTask();
+		}
+		else {
+			console.log("-------------------------")
+			console.log("Please specify the text file you would like to read commands from after you request 'do-what-it-says'.");
+			console.log("-------------------------")
+		}
 		break
 
-	default:
-		console.log("please enter a valid command");
+		default:
+		console.log("-------------------------")
+		console.log("Please enter a valid command.");
+		console.log("-------------------------")
 		break
+
+	}
 }
 
+readInput();
